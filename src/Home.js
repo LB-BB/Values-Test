42857f0bc0360c46692ab5a3b9a87d8d646dcbad
LB-BB <57300241+LB-BB@users.noreply.github.com>
2024-07-11 18:34:34 -0700

    update start button & home text

--- src/Home.js
+++ src/Home.js
@@ -13,6 +13,7 @@ const Home = () => {
     <div className="home-container">
       <div className='start-container'>
         <h1 className="title">Welcome to the Values Card Sort</h1>
+        <h3 className='subtitle'>Use this quiz to determine your top ten personal values.</h3>
         <h3 className='subtitle'>Based on the card set by W.R. Miller, J. C’de Baca, D.B. Matthews, P.L.Wilbourne</h3>
         <h4 className='subtitle subtitle-2'>at the University of New Mexico, 2001</h4>
         <button className="start-button" onClick={handleStart}>
